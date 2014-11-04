65480757883f92476e320ac02820af9e59075a8f
anderschen <chenhf2010@qq.com>
2014-11-04 17:50:57 +0800

    commit

--- src/main/java/com/cssweb/payment/posp/business/BusiApplyKey.java
+++ src/main/java/com/cssweb/payment/posp/business/BusiApplyKey.java
@@ -1,9 +1,12 @@
 package com.cssweb.payment.posp.business;
 
 
-import com.cssweb.payment.posp.network.CustomMessage;
+import com.cssweb.payment.posp.network.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chenhf on 2014/10/15.
@@ -17,6 +20,31 @@ public class BusiApplyKey implements BusinessAction {
 
         logger.info("==处理申请密钥==");
 
-        return null;
+        CustomMessage response = new CustomMessage();
+        MsgHeader msgHeader = new MsgHeader();
+        MessageType msgType = new MessageType();
+        BitFieldMap bitFieldMap = new BitFieldMap();
+        FieldData fieldData = new FieldData();
+        List<Field> fields = new ArrayList<Field>();
+
+
+        try {
+            Field3 field3 = new Field3();
+
+            field3.setData("00X000");
+            System.out.println(field3.toString());
+            fields.add(field3);
+
+        } catch (OverflowMaxLengthException e) {
+            e.printStackTrace();
+        } catch (FieldLengthException e) {
+            e.printStackTrace();
+        }
+
+
+
+        msgType.setMsgType("0830");
+
+        return response;
     }
 }
